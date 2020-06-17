413f98a34c641851314a08403067735e2bbfedb0
= <zainr7989@gmail.com>
2020-06-17 10:57:50 -0400

    Init numIslands solution.

--- challenges.py
+++ challenges.py
@@ -56,8 +56,60 @@ def timeToRot(grid):
     # else return minutes
     return minutes
 
+def numIslands(grid):
+    """Take in a grid of 1s (land) and 0s (water) and return the number of islands."""
+    # init a counter for the number of land masses
+    islands = 0
+    # Idea 1: each island is a set of connected components in the overall grid
+    # return the number of islands
+    return islands
+
 
 if __name__ == "__main__":
     pass
-     
-        
+
+
+# code for using DFS to find all CC from before
+def dfs_for_cc(self, vertex, visited, connected):
+    """Returns a list of all verticies within one set of
+        connected components in a Graph object.
+
+        Parameters:
+        vertex(Vertex): the vertex we begin with
+        visited(set): all other Vertex objects visited in the overall
+                        graph so far
+        connected(list): collection of vertices visted so far in this
+                        set of connected components
+
+        Returns: list: all verticies within one set of
+        connected components in a graph
+    
+    """
+    # visit this vertex
+    visited.add(vertex)
+    connected.append(vertex.get_id())
+    # iterate over neighbors
+    neighbors = vertex.get_neighbors()
+    for n in neighbors:
+        if n not in visited:
+            self.dfs_for_cc(n, visited, connected)
+    return connected
+
+def find_connected_components(self):
+    """Return a 2D list of connected components.
+        Each of the inner lists contains vertex ids.
+        A connected component of a graph is a set
+        of vertices for which there is a path between any pair of vertices.
+
+    """
+    # set for all previous seen vertices
+    visited = set()
+    # execute DFS - find all connected components
+    all_connected_components = list()
+    for vertex in self.__vertex_dict.values():
+        if vertex not in visited:
+            components = list()
+            self.dfs_for_cc(vertex, visited, components)
+            all_connected_components.append(components)
+    # return the connected components
+    return all_connected_components
